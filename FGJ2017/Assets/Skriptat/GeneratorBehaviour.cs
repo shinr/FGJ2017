e06a1a33e48f649657ea8833ec08bf9f1d2ae925
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2017-01-21 18:15:11 +0200

    ObstacleSpawner

---
+++ FGJ2017/Assets/Skriptat/GeneratorBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorBehaviour : MonoBehaviour {
+    public GameObject obstacle;
+
+    float spawnTimer = 1.0f;
+
+    enum Levels
+    {
+        Ground, Air
+    }
+    Levels currentLevel;
+
+	// Use this for initialization
+	void Start () {
+        currentLevel = Levels.Ground;
+        Invoke("SpawnNewObstacle", spawnTimer);
+	}
+
+    void SpawnNewObstacle() {
+        Instantiate(obstacle, this.transform.position, Quaternion.identity);
+        Invoke("SpawnNewObstacle", spawnTimer);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FGJ2017/Assets/Skriptat/ObstacleMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour {
+    public float speed;
+    private Vector3 direction = Vector3.left;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("moi kollasin");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void FixedUpdate() {
+        this.transform.Translate(direction * speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
