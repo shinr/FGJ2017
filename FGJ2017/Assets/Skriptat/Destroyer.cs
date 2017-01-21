7187398f2deb7609524ef41e4782581fad5a8e43
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2017-01-21 21:15:06 +0200

    destroyer works

---
+++ FGJ2017/Assets/Skriptat/Destroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("moi kollasin" + coll.gameObject.name);
+
+        if (coll.gameObject.name.Contains("Destroyer"))
+        {
+            Debug.Log("Destryoed!");
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
